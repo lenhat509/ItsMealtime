0ab7b69d1934317e038e47d475baf2a6209f01d5
Nhat Le <nhat.n.le@sjsu.edu>
2023-05-16 22:53:14 -0700

    set up kubernetes and skafford

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express()
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+    console.log("Listening to 3000 from Auth")
+})
